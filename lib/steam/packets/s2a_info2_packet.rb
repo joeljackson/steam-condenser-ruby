4f934340dfeaa8e31020a482c4f751b99c879c7d
Rodrigo Navarro <rnavarro1@gmail.com>
2012-01-01 21:57:38 -0200

    Moving erros, packets and webapi to SteamCondenser module

--- lib/steam/packets/s2a_info2_packet.rb
+++ lib/steam/packets/s2a_info2_packet.rb
@@ -13,62 +13,64 @@ require 'steam/packets/s2a_info_base_packet'
 #
 # @author Sebastian Staudt
 # @see GameServer#update_server_info
-class S2A_INFO2_Packet
+module SteamCondenser
+  class S2A_INFO2_Packet
 
-  include S2A_INFO_BasePacket
+    include S2A_INFO_BasePacket
 
-  EDF_GAME_ID     = 0x01
-  EDF_GAME_PORT   = 0x80
-  EDF_SERVER_ID   = 0x10
-  EDF_SERVER_TAGS = 0x20
-  EDF_SOURCE_TV   = 0x40
+    EDF_GAME_ID     = 0x01
+    EDF_GAME_PORT   = 0x80
+    EDF_SERVER_ID   = 0x10
+    EDF_SERVER_TAGS = 0x20
+    EDF_SOURCE_TV   = 0x40
 
-  # Creates a new S2A_INFO2 response object based on the given data
-  #
-  # @param [String] data The raw packet data replied from the server
-  # @see S2A_INFO_BasePacket#generate_info_hash
-  def initialize(data)
-    super S2A_INFO2_HEADER, data
+    # Creates a new S2A_INFO2 response object based on the given data
+    #
+    # @param [String] data The raw packet data replied from the server
+    # @see S2A_INFO_BasePacket#generate_info_hash
+    def initialize(data)
+      super S2A_INFO2_HEADER, data
 
-    info[:protocol_version] = @content_data.byte
-    info[:server_name] = @content_data.cstring
-    info[:map_name] = @content_data.cstring
-    info[:game_directory] = @content_data.cstring
-    info[:game_description] = @content_data.cstring
-    info[:app_id] = @content_data.short
-    info[:number_of_players] = @content_data.byte
-    info[:max_players] = @content_data.byte
-    info[:number_of_bots] = @content_data.byte
-    info[:dedicated] = @content_data.byte.chr
-    info[:operating_system] = @content_data.byte.chr
-    info[:password_needed] = @content_data.byte == 1
-    info[:secure] = @content_data.byte == 1
-    info[:game_version] = @content_data.cstring
+      info[:protocol_version] = @content_data.byte
+      info[:server_name] = @content_data.cstring
+      info[:map_name] = @content_data.cstring
+      info[:game_directory] = @content_data.cstring
+      info[:game_description] = @content_data.cstring
+      info[:app_id] = @content_data.short
+      info[:number_of_players] = @content_data.byte
+      info[:max_players] = @content_data.byte
+      info[:number_of_bots] = @content_data.byte
+      info[:dedicated] = @content_data.byte.chr
+      info[:operating_system] = @content_data.byte.chr
+      info[:password_needed] = @content_data.byte == 1
+      info[:secure] = @content_data.byte == 1
+      info[:game_version] = @content_data.cstring
 
-    if @content_data.remaining > 0
-      extra_data_flag = @content_data.byte
+      if @content_data.remaining > 0
+        extra_data_flag = @content_data.byte
 
-      if extra_data_flag & EDF_GAME_PORT != 0
-        info[:server_port] = @content_data.short
-      end
+        if extra_data_flag & EDF_GAME_PORT != 0
+          info[:server_port] = @content_data.short
+        end
 
-      if extra_data_flag & EDF_SERVER_ID != 0
-        info[:server_id] = @content_data.long | (@content_data.long << 32)
-      end
+        if extra_data_flag & EDF_SERVER_ID != 0
+          info[:server_id] = @content_data.long | (@content_data.long << 32)
+        end
 
-      if extra_data_flag & EDF_SOURCE_TV != 0
-        info[:tv_port] = @content_data.short
-        info[:tv_name] = @content_data.cstring
-      end
+        if extra_data_flag & EDF_SOURCE_TV != 0
+          info[:tv_port] = @content_data.short
+          info[:tv_name] = @content_data.cstring
+        end
 
-      if extra_data_flag & EDF_SERVER_TAGS != 0
-        info[:server_tags] = @content_data.cstring
-      end
+        if extra_data_flag & EDF_SERVER_TAGS != 0
+          info[:server_tags] = @content_data.cstring
+        end
 
-      if extra_data_flag & EDF_GAME_ID != 0
-        info[:game_id] = @content_data.long | (@content_data.long << 32)
+        if extra_data_flag & EDF_GAME_ID != 0
+          info[:game_id] = @content_data.long | (@content_data.long << 32)
+        end
       end
     end
-  end
 
+  end
 end
--- lib/steam/packets/s2a_info_detailed_packet.rb
+++ lib/steam/packets/s2a_info_detailed_packet.rb
@@ -13,47 +13,49 @@ require 'steam/packets/s2a_info_base_packet'
 #             format. Newer ones use the same format as Source servers now (see
 #             {S2A_INFO2_Packet}).
 # @see GameServer#update_server_info
-class S2A_INFO_DETAILED_Packet
+module SteamCondenser
+  class S2A_INFO_DETAILED_Packet
 
-  include S2A_INFO_BasePacket
+    include S2A_INFO_BasePacket
 
-  # Creates a new S2A_INFO_DETAILED response object based on the given data
-  #
-  # @param [String] data The raw packet data replied from the server
-  # @see S2A_INFO_BasePacket#generate_info_hash
-  def initialize(data)
-    super S2A_INFO_DETAILED_HEADER, data
+    # Creates a new S2A_INFO_DETAILED response object based on the given data
+    #
+    # @param [String] data The raw packet data replied from the server
+    # @see S2A_INFO_BasePacket#generate_info_hash
+    def initialize(data)
+      super S2A_INFO_DETAILED_HEADER, data
 
-    info[:game_ip] = @content_data.cstring
-    info[:server_name] = @content_data.cstring
-    info[:map_name] = @content_data.cstring
-    info[:game_directory] = @content_data.cstring
-    info[:game_description] = @content_data.cstring
-    info[:number_of_players] = @content_data.byte
-    info[:max_players] = @content_data.byte
-    info[:network_version] = @content_data.byte
-    info[:dedicated] = @content_data.byte.chr
-    info[:operating_system] = @content_data.byte.chr
-    info[:password_needed] = @content_data.byte == 1
-    info[:is_mod] = @content_data.byte == 1
+      info[:game_ip] = @content_data.cstring
+      info[:server_name] = @content_data.cstring
+      info[:map_name] = @content_data.cstring
+      info[:game_directory] = @content_data.cstring
+      info[:game_description] = @content_data.cstring
+      info[:number_of_players] = @content_data.byte
+      info[:max_players] = @content_data.byte
+      info[:network_version] = @content_data.byte
+      info[:dedicated] = @content_data.byte.chr
+      info[:operating_system] = @content_data.byte.chr
+      info[:password_needed] = @content_data.byte == 1
+      info[:is_mod] = @content_data.byte == 1
 
-    if info[:is_mod]
-      info[:mod_info] = {}
-      info[:mod_info][:url_info] = @content_data.cstring
-      info[:mod_info][:url_dl] = @content_data.cstring
-      @content_data.byte
-      if @content_data.remaining == 12
-        info[:mod_info][:mod_version] = @content_data.long
-        info[:mod_info][:mod_size] = @content_data.long
-        info[:mod_info][:sv_only] = @content_data.byte == 1
-        info[:mod_info][:cl_dll] = @content_data.byte == 1
+      if info[:is_mod]
+        info[:mod_info] = {}
+        info[:mod_info][:url_info] = @content_data.cstring
+        info[:mod_info][:url_dl] = @content_data.cstring
+        @content_data.byte
+        if @content_data.remaining == 12
+          info[:mod_info][:mod_version] = @content_data.long
+          info[:mod_info][:mod_size] = @content_data.long
+          info[:mod_info][:sv_only] = @content_data.byte == 1
+          info[:mod_info][:cl_dll] = @content_data.byte == 1
+          info[:secure] = @content_data.byte == 1
+          info[:number_of_bots] = @content_data.byte
+        end
+      else
         info[:secure] = @content_data.byte == 1
         info[:number_of_bots] = @content_data.byte
       end
-    else
-      info[:secure] = @content_data.byte == 1
-      info[:number_of_bots] = @content_data.byte
     end
-  end
 
+  end
 end
