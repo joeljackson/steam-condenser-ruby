b057e8e5e0c44ed9a3a807672c700dd4badb5035
Sebastian Staudt <koraktor@gmail.com>
2012-08-21 07:22:20 +0200

    Moved community classes into an own namespace

---
+++ lib/steam-condenser/community/alien_swarm/alien_swarm_stats.rb
@@ -0,0 +1,190 @@
+# This code is free software; you can redistribute it and/or modify it under
+# the terms of the new BSD License.
+#
+# Copyright (c) 2010-2012, Sebastian Staudt
+
+require 'steam-condenser/community/alien_swarm/alien_swarm_mission'
+require 'steam-condenser/community/alien_swarm/alien_swarm_weapon'
+
+module SteamCondenser::Community
+
+  # This class represents the game statistics for a single user in Alien Swarm
+  #
+  # @author Sebastian Staudt
+  class AlienSwarmStats < GameStats
+
+    # Returns general stats for the players
+    #
+    # @return [Hash<Symbol, Object>] The stats for the player
+    attr_reader :lifetime_stats
+
+    # The base URL for all images referenced in the stats
+    BASE_URL = 'http://cdn.steamcommunity.com/public/images/gamestats/swarm/'
+
+    # The names of all weapons in Alien Swarm
+    WEAPONS = [ 'Autogun', 'Cannon_Sentry', 'Chainsaw', 'Flamer',
+                'Grenade_Launcher', 'Hand_Grenades', 'Hornet_Barrage',
+                'Incendiary_Sentry', 'Laser_Mines', 'Marskman_Rifle', 'Minigun',
+                'Mining_Laser', 'PDW', 'Pistol', 'Prototype_Rifle', 'Rail_Rifle',
+                'Rifle', 'Rifle_Grenade', 'Sentry_Gun', 'Shotgun',
+                'Tesla_Cannon', 'Vindicator', 'Vindicator_Grenade' ]
+
+    # Creates a new `AlienSwarmStats` instance by calling the super constructor
+    # with the game name `'alienswarm'`
+    #
+    # @param [String, Fixnum] steam_id The custom URL or the 64bit Steam ID of
+    #        the user
+    # @macro cacheable
+    def initialize(steam_id)
+      super steam_id, 'alienswarm'
+
+      if public?
+        lifetime_data = @xml_data['stats']['lifetime']
+
+        @hours_played = lifetime_data['timeplayed']
+
+        @lifetime_stats = {}
+        @lifetime_stats[:accuracy]            = lifetime_data['accuracy'].to_f
+        @lifetime_stats[:aliens_burned]       = lifetime_data['aliensburned'].to_i
+        @lifetime_stats[:aliens_killed]       = lifetime_data['alienskilled'].to_i
+        @lifetime_stats[:campaigns]           = lifetime_data['campaigns'].to_i
+        @lifetime_stats[:damage_taken]        = lifetime_data['damagetaken'].to_i
+        @lifetime_stats[:experience]          = lifetime_data['experience'].to_i
+        @lifetime_stats[:experience_required] = lifetime_data['xprequired'].to_i
+        @lifetime_stats[:fast_hacks]          = lifetime_data['fasthacks'].to_i
+        @lifetime_stats[:friendly_fire]       = lifetime_data['friendlyfire'].to_i
+        @lifetime_stats[:games_successful]    = lifetime_data['gamessuccess'].to_i
+        @lifetime_stats[:healing]             = lifetime_data['healing'].to_i
+        @lifetime_stats[:kills_per_hour]      = lifetime_data['killsperhour'].to_f
+        @lifetime_stats[:level]               = lifetime_data['level'].to_i
+        @lifetime_stats[:promotion]           = lifetime_data['promotion'].to_i
+        @lifetime_stats[:promotion_img]       = BASE_URL + lifetime_data['promotionpic'] if @lifetime_stats[:promotion] > 0
+        @lifetime_stats[:next_unlock]         = lifetime_data['nextunlock']
+        @lifetime_stats[:next_unlock_img]     = BASE_URL + lifetime_data['nextunlockimg']
+        @lifetime_stats[:shots_fired]         = lifetime_data['shotsfired'].to_i
+        @lifetime_stats[:total_games]         = lifetime_data['totalgames'].to_i
+
+        @lifetime_stats[:games_successful_percentage] = (@lifetime_stats[:total_games] > 0) ? @lifetime_stats[:games_successful].to_f / @lifetime_stats[:total_games] : 0;
+      end
+    end
+
+    # Returns the favorites of this user like weapons and marine
+    #
+    # If the favorites haven't been parsed already, parsing is done now.
+    #
+    # @return [Hash<Symbol, Object>] The favorites of this player
+    def favorites
+      return unless public?
+
+      if @favorites.nil?
+        favorites_data = @xml_data['stats']['favorites']
+
+        @favorites = {}
+        @favorites[:class]                       = favorites_data['class']
+        @favorites[:class_img]                   = favorites_data['classimg']
+        @favorites[:class_percentage]            = favorites_data['classpct'].to_f
+        @favorites[:difficulty]                  = favorites_data['difficulty']
+        @favorites[:difficulty_percentage]       = favorites_data['difficultypct'].to_f
+        @favorites[:extra]                       = favorites_data['extra']
+        @favorites[:extra_img]                   = favorites_data['extraimg']
+        @favorites[:extra_percentage]            = favorites_data['extrapct'].to_f
+        @favorites[:marine]                      = favorites_data['marine']
+        @favorites[:marine_img]                  = favorites_data['marineimg']
+        @favorites[:marine_percentage]           = favorites_data['marinepct'].to_f
+        @favorites[:mission]                     = favorites_data['mission']
+        @favorites[:mission_img]                 = favorites_data['missionimg']
+        @favorites[:mission_percentage]          = favorites_data['missionpct'].to_f
+        @favorites[:primary_weapon]              = favorites_data['primary']
+        @favorites[:primary_weapon_img]          = favorites_data['primaryimg']
+        @favorites[:primary_weapon_percentage]   = favorites_data['primarypct'].to_f
+        @favorites[:secondary_weapon]            = favorites_data['secondary']
+        @favorites[:secondary_weapon_img]        = favorites_data['secondaryimg']
+        @favorites[:secondary_weapon_percentage] = favorites_data['secondarypct'].to_f
+      end
+
+      @favorites
+    end
+
+    # Returns the item stats for this user like ammo deployed and medkits
+    # used
+    #
+    # If the items haven't been parsed already, parsing is done now.
+    #
+    # @return [Hash<Symbol, Object>] The item stats of this player
+    def item_stats
+      return unless public?
+
+      if @item_stats.nil?
+        weapons_data = @xml_data['stats']['weapons']
+
+        @item_stats = {}
+        @item_stats[:ammo_deployed]             = weapons_data['ammo_deployed'].to_i
+        @item_stats[:sentryguns_deployed]       = weapons_data['sentryguns_deployed'].to_i
+        @item_stats[:sentry_flamers_deployed]   = weapons_data['sentry_flamers_deployed'].to_i
+        @item_stats[:sentry_freeze_deployed]    = weapons_data['sentry_freeze_deployed'].to_i
+        @item_stats[:sentry_cannon_deployed]    = weapons_data['sentry_cannon_deployed'].to_i
+        @item_stats[:medkits_used]              = weapons_data['medkits_used'].to_i
+        @item_stats[:flares_used]               = weapons_data['flares_used'].to_i
+        @item_stats[:adrenaline_used]           = weapons_data['adrenaline_used'].to_i
+        @item_stats[:tesla_traps_deployed]      = weapons_data['tesla_traps_deployed'].to_i
+        @item_stats[:freeze_grenades_thrown]    = weapons_data['freeze_grenades_thrown'].to_i
+        @item_stats[:electric_armor_used]       = weapons_data['electric_armor_used'].to_i
+        @item_stats[:healgun_heals]             = weapons_data['healgun_heals'].to_i
+        @item_stats[:healgun_heals_self]        = weapons_data['healgun_heals_self'].to_i
+        @item_stats[:healbeacon_heals]          = weapons_data['healbeacon_heals'].to_i
+        @item_stats[:healbeacon_heals_self]     = weapons_data['healbeacon_heals_self'].to_i
+        @item_stats[:damage_amps_used]          = weapons_data['damage_amps_used'].to_i
+        @item_stats[:healbeacons_deployed]      = weapons_data['healbeacons_deployed'].to_i
+        @item_stats[:healbeacon_heals_pct]      = weapons_data['healbeacon_heals_pct'].to_f
+        @item_stats[:healgun_heals_pct]         = weapons_data['healgun_heals_pct'].to_f
+        @item_stats[:healbeacon_heals_pct_self] = weapons_data['healbeacon_heals_pct_self'].to_f
+        @item_stats[:healgun_heals_pct_self]    = weapons_data['healgun_heals_pct_self'].to_f
+      end
+
+      @item_stats
+    end
+
+    # Returns the stats for individual missions for this user containing all
+    # Alien Swarm missions
+    #
+    # If the mission stats haven't been parsed already, parsing is done now.
+    #
+    # @return [Hash<String, AlienSwarmMission>] The mission stats for this
+    #         player
+    def mission_stats
+      return unless public?
+
+      if @mission_stats.nil?
+        @mission_stats = {}
+        @xml_data['stats']['missions'].each do |mission_data|
+          mission = AlienSwarmMission.new *mission_data
+          @mission_stats[mission.name] = mission
+        end
+      end
+
+      @mission_stats
+    end
+
+    # Returns the stats for individual weapons for this user containing all
+    # Alien Swarm weapons
+    #
+    # If the weapon stats haven't been parsed already, parsing is done now.
+    #
+    # @return [Hash<String, AlienSwarmWeapon>] The weapon stats for this player
+    def weapon_stats
+      return unless public?
+
+      if @weapon_stats.nil?
+        @weapon_stats = {}
+        WEAPONS.each do |weapon_node|
+          weapon_data = @xml_data['stats']['weapons'][weapon_node]
+          weapon = AlienSwarmWeapon.new(weapon_data)
+          @weapon_stats[weapon.name] = weapon
+        end
+      end
+
+      @weapon_stats
+    end
+
+  end
+end
---
+++ lib/steam-condenser/community/css/css_stats.rb
@@ -0,0 +1,145 @@
+# This code is free software; you can redistribute it and/or modify it under
+# the terms of the new BSD License.
+#
+# Copyright (c) 2010-2013, Sebastian Staudt
+
+require 'steam-condenser/community/css/css_map'
+require 'steam-condenser/community/css/css_weapon'
+
+module SteamCondenser::Community
+
+  # The is class represents the game statistics for a single user in
+  # Counter-Strike: Source
+  #
+  # @author Sebastian Staudt
+  class CSSStats < GameStats
+
+    # The names of the maps in Counter-Strike: Source
+    MAPS = [ 'cs_assault', 'cs_compound', 'cs_havana', 'cs_italy', 'cs_militia',
+             'cs_office', 'de_aztec', 'de_cbble', 'de_chateau', 'de_dust',
+             'de_dust2', 'de_inferno', 'de_nuke', 'de_piranesi', 'de_port',
+             'de_prodigy', 'de_tides', 'de_train' ]
+
+    # The names of the weapons in Counter-Strike: Source
+    WEAPONS = [ 'deagle', 'usp', 'glock', 'p228', 'elite', 'fiveseven', 'awp',
+                'ak47', 'm4a1', 'aug', 'sg552', 'sg550', 'galil', 'famas',
+                'scout', 'g3sg1', 'p90', 'mp5navy', 'tmp', 'mac10', 'ump45',
+                'm3', 'xm1014', 'm249', 'knife', 'grenade' ]
+
+    # Returns statistics about the last match the player played
+    #
+    # @return [Hash<Symbol, Object>] The stats of the last match
+    attr_reader :last_match_stats
+
+    # Returns overall statistics of this player
+    #
+    # @return [Hash<Symbol, Object>] The overall statistics
+    attr_reader :total_stats
+
+    # Creates a `CSSStats` instance by calling the super constructor
+    # with the game name `'cs:s'`
+    #
+    # @param [String, Fixnum] steam_id The custom URL or 64bit Steam ID of the
+    #        user
+    # @macro cacheable
+    def initialize(steam_id)
+      super steam_id, 'cs:s'
+
+      if public?
+        last_match_data = @xml_data['stats']['lastmatch']
+        lifetime_data = @xml_data['stats']['lifetime']
+        summary_data = @xml_data['stats']['summary']
+
+        @last_match_stats = {}
+        @total_stats      = {}
+
+        @last_match_stats[:cost_per_kill]      = last_match_data['costkill'].to_f
+        @last_match_stats[:ct_wins]            = last_match_data['ct_wins'].to_i
+        @last_match_stats[:damage]             = last_match_data['dmg'].to_i
+        @last_match_stats[:deaths]             = last_match_data['deaths'].to_i
+        @last_match_stats[:dominations]        = last_match_data['dominations'].to_i
+        @last_match_stats[:favorite_weapon_id] = last_match_data['favwpnid'].to_i
+        @last_match_stats[:kills]              = last_match_data['kills'].to_i
+        @last_match_stats[:max_players]        = last_match_data['max_players'].to_i
+        @last_match_stats[:money]              = last_match_data['money'].to_i
+        @last_match_stats[:revenges]           = last_match_data['revenges'].to_i
+        @last_match_stats[:stars]              = last_match_data['stars'].to_i
+        @last_match_stats[:t_wins]             = last_match_data['t_wins'].to_i
+        @last_match_stats[:wins]               = last_match_data['wins'].to_i
+
+        @total_stats[:blind_kills]             = lifetime_data['blindkills'].to_i
+        @total_stats[:bombs_defused]           = lifetime_data['bombsdefused'].to_i
+        @total_stats[:bombs_planted]           = lifetime_data['bombsplanted'].to_i
+        @total_stats[:damage]                  = lifetime_data['dmg'].to_i
+        @total_stats[:deaths]                  = summary_data['deaths'].to_i
+        @total_stats[:domination_overkills]    = lifetime_data['dominationoverkills'].to_i
+        @total_stats[:dominations]             = lifetime_data['dominations'].to_i
+        @total_stats[:earned_money]            = lifetime_data['money'].to_i
+        @total_stats[:enemy_weapon_kills]      = lifetime_data['enemywpnkills'].to_i
+        @total_stats[:headshots]               = lifetime_data['headshots'].to_i
+        @total_stats[:hits]                    = summary_data['shotshit'].to_i
+        @total_stats[:hostages_rescued]        = lifetime_data['hostagesrescued'].to_i
+        @total_stats[:kills]                   = summary_data['kills'].to_i
+        @total_stats[:knife_kills]             = lifetime_data['knifekills'].to_i
+        @total_stats[:logos_sprayed]           = lifetime_data['decals'].to_i
+        @total_stats[:nightvision_damage]      = lifetime_data['nvgdmg'].to_i
+        @total_stats[:pistol_rounds_won]       = lifetime_data['pistolrounds'].to_i
+        @total_stats[:revenges]                = lifetime_data['revenges'].to_i
+        @total_stats[:rounds_played]           = summary_data['rounds'].to_i
+        @total_stats[:rounds_won]              = summary_data['wins'].to_i
+        @total_stats[:seconds_played]          = summary_data['timeplayed'].to_i
+        @total_stats[:shots]                   = summary_data['shots'].to_i
+        @total_stats[:stars]                   = summary_data['stars'].to_i
+        @total_stats[:weapons_donated]         = lifetime_data['wpndonated'].to_i
+        @total_stats[:windows_broken]          = lifetime_data['winbroken'].to_i
+        @total_stats[:zoomed_sniper_kills]     = lifetime_data['zsniperkills'].to_i
+
+        @last_match_stats[:kdratio] = (@total_stats[:deaths] > 0) ? @last_match_stats[:kills].to_f / @last_match_stats[:deaths] : 0
+        @total_stats[:accuracy]     = (@total_stats[:shots] > 0) ? @total_stats[:hits].to_f / @total_stats[:shots] : 0
+        @total_stats[:kdratio]      = (@total_stats[:deaths] > 0) ? @total_stats[:kills].to_f / @total_stats[:deaths] : 0
+        @total_stats[:rounds_lost]  = @total_stats[:rounds_played] - @total_stats[:rounds_won]
+      end
+    end
+
+    # Returns a map of `CSSMap` for this user containing all CS:S maps.
+    #
+    # If the maps haven't been parsed already, parsing is done now.
+    #
+    # @return [Hash<String, Object>] The map statistics for this user
+    def map_stats
+      return unless public?
+
+      if @map_stats.nil?
+        @map_stats = {}
+        maps_data = @xml_data['stats']['maps']
+
+        MAPS.each do |map_name|
+          @map_stats[map_name] = CSSMap.new(map_name, maps_data)
+        end
+      end
+
+      @map_stats
+    end
+
+    # Returns a map of `CSSWeapon` for this user containing all CS:S weapons.
+    #
+    # If the weapons haven't been parsed already, parsing is done now.
+    #
+    # @return [Hash<String, Object>] The weapon statistics for this user
+    def weapon_stats
+      return unless public?
+
+      if @weapon_stats.nil?
+        @weapon_stats = {}
+        weapons_data = @xml_data['stats']['weapons']
+
+        WEAPONS.each do |weapon_name|
+          @weapon_stats[weapon_name] = CSSWeapon.new(weapon_name, weapons_data)
+        end
+      end
+
+      @weapon_stats
+    end
+
+  end
+end
